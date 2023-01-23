3d1aca621f1ef05a6d7ad020994481e5a52ffecc
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2023-01-23 12:36:29 -0500

    send index

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/api', require('./api'));
 
 app.use('/', (req, res, next) => {
   try {
-    res.sendFile(path.join(__dirname, 'client', 'dist'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   } catch (error) {
     next(error);
   }
