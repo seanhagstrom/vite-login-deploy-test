c3deb8fd548301a7233ffd6578835c94e0d63a68
Sean Hagstrom <sean.hagstrom@fullstackacademy.com>
2023-01-23 11:32:13 -0500

    force a pr

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './client', 'dist')));
 
+// add to make new PR
 app.use('/api', require('./api'));
 
 app.use((req, res, next) => {
